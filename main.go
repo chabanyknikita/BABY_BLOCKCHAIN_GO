9931314c33aea1ec7454778e86ed6e12350c6630
Nikita Chabaniuk <92546152+chabanyknikita@users.noreply.github.com>
2022-11-29 21:09:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/ecdsa"
+	"encoding/hex"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -11,6 +12,19 @@ import (
 type KeyPair struct {
 	PrivateKey []byte
 	PublicKey  []byte
+}
+
+type Account struct {
+	accountID string
+	wallet    KeyPair
+	balance   int
+}
+
+type Operation struct {
+	sender    Account
+	receiver  Account
+	amount    int
+	signature []byte
 }
 
 func genKeyPair() KeyPair {
@@ -84,3 +98,64 @@ func printSignature() {
 	signature := signData(genKeyPair().PrivateKey, "")
 	fmt.Println("SIGNATURE:   ", hexutil.Encode(signature))
 }
+
+func genAccount() Account {
+	//wallet
+	keys := genKeyPair()
+
+	// accountID
+	dst := make([]byte, hex.EncodedLen(len(keys.PublicKey)))
+	hex.Encode(dst, keys.PublicKey)
+	accountID := fmt.Sprintf("%s\n", dst)
+
+	return Account{
+		accountID: accountID,
+		wallet:    keys,
+		balance:   0,
+	}
+
+}
+
+func (acc *Account) addKeyPairToWallet(pair KeyPair) {
+	acc.wallet = pair
+}
+
+func (acc *Account) updateBalance(balance int) {
+	acc.balance = balance
+}
+
+func (acc *Account) getBalance() int {
+	return acc.balance
+}
+
+func (acc *Account) printBalance() {
+	fmt.Println(acc.balance)
+}
+
+func (acc *Account) signDataAcc(message string) []byte {
+	privateK := acc.wallet.PrivateKey
+	privateKey, err := crypto.HexToECDSA(hexutil.Encode(privateK)[2:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := []byte(message)
+	hash := crypto.Keccak256Hash(data)
+
+	signature, err := crypto.Sign(hash.Bytes(), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return signature
+}
+
+func (acc *Account) createOperation(recipient Account, amount int, index int) Operation {
+	return Operation{
+		sender:    *acc,
+		receiver:  recipient,
+		amount:    amount,
+		signature: Operation{}.signature,
+	}
+}
+
